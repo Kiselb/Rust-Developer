ea6bb85d0a6a24e550e6189e6599f6ac33509cb3
Киселев Михаил В <mvkiselev@group.legion.ru>
2022-05-26 18:08:11 +0300

    Solution hw#2

---
+++ smart_house/src/main.rs
@@ -0,0 +1,50 @@
+struct _ElectricSocket {
+    name: String,
+    power_consumption: u32,
+    status: bool,
+}
+struct _Thermometer {
+    name: String,
+    temperature: i8,
+}
+impl _ElectricSocket {
+    pub fn _new(name: String) -> Self {
+        Self {
+            name,
+            power_consumption: 0,
+            status: false,
+        }
+    }
+    pub fn _on(&self) {
+        todo!()
+    }
+    pub fn _off(&self) {
+        todo!()
+    }
+    pub fn _name(&self) -> String {
+        todo!()
+    }
+    pub fn _power(&self) -> u32 {
+        todo!()
+    }
+    pub fn _status(&self) -> bool {
+        todo!()
+    }
+}
+impl _Thermometer {
+    pub fn _new(name: String) -> Self {
+        Self {
+            name,
+            temperature: 0,
+        }
+    }
+    pub fn _name(&self) -> String {
+        todo!()
+    }
+    pub fn _value(&self) -> i8 {
+        todo!()
+    }
+}
+fn main() {
+    todo!()
+}
