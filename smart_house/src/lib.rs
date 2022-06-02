490d4f2c5503521263bebe5d8c82f20a2ff80d6d
Киселев Михаил В <mvkiselev@group.legion.ru>
2022-06-02 13:43:33 +0300

    Решение домашнего задания №3 исправление ошибок

--- smart_house/src/lib.rs
+++ smart_house/src/lib.rs
@@ -59,9 +59,9 @@ impl SmartRoom {
     pub fn add(&mut self, device: Box<dyn SmartDevice>) {
         self.devices.insert(String::from(device.identity()), device);
     }
-    #[allow(clippy::borrowed_box)]
-    pub fn get(&self, device_name: &str) -> Option<&Box<dyn SmartDevice>> {
-        self.devices.get(device_name)
+    //#[allow(clippy::borrowed_box)]
+    pub fn get(&self, device_name: &str) -> Option<&dyn SmartDevice> {
+        self.devices.get(device_name).map(|v| v.as_ref())
     }
 }
 pub trait SmartDevice {
